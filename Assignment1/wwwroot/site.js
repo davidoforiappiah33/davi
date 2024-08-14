a8a0daabe2b990e21b7382f5818c4443f4290fcc
davidoforiappiah33 <oforiappiahdavid33@gmail.com>
2024-08-14 23:01:02 +0000

    Add files via upload

---
+++ Assignment1/wwwroot/site.js
@@ -0,0 +1,9 @@
+﻿window.initializeSidebar = function () {
+    const navLinks = document.querySelectorAll('#sidebarMenu .nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            navLinks.forEach(nav => nav.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+};
